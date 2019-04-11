a612cb70fea282864ca4b11cb8dc6242134acbd8
HamSeungPyo <39762906+HamSeungPyo@users.noreply.github.com>
2019-04-11 18:40:13 +0900

    이미지 셋팅 및 충돌체 콘트롤러작성

---
+++ Assets/Script/JudgingRangeControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JudgingRangeControl : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.transform.tag == "Enemy")
+        {
+            float dis = Mathf.Abs(transform.position.x - col.transform.position.x);
+            Debug.Log(dis);
+            Destroy(col.gameObject);
+        }
+    }
+}
---
+++ Assets/Script/PlayerControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public Animator ainm;
+
+    public void Attack()
+    {
+        ainm.SetTrigger("Attack");
+    }
+}
